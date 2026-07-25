c6bb28eb57f4910ceabbf93fa93c2de542ed1ece
OBozova <59206880+OBozova@users.noreply.github.com>
2026-07-25 09:35:31 +0300

    feat: test coverage report script added

--- packages/backend/vitest.config.ts
+++ packages/backend/vitest.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from "vitest/config";
-import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [vue()],
   test: {
-    environment: "jsdom",
     coverage: {
       provider: "v8",
       reporter: ["text", "html"],
-      include: ["src/**/*.{ts,vue}"],
+      include: ["src/**/*.ts"],
     },
   },
 });
